2c234882e3df2b5d379f2b6f321fd418515fc35d
Mikkel Strange <mikkel.strange@gmail.com>
2020-12-05 22:17:33 +0100

    part 2 og december

--- dec3/dec3.py
+++ dec3/dec3.py
@@ -1,12 +1,7 @@
+from functools import reduce
 
 
 def periodic(xs):
-    '''
-    >>> periodic('abc')(3)
-    'a'
-    >>> periodic('abc')(-1)
-    'c'
-    '''
     n = len(xs)
     return lambda i: xs[i % n]
 
@@ -17,21 +12,19 @@ def dec3(rows):
     >>> dec3(['..##.......',  '#...#...#..', '.#....#..#.', '..#.#...#.#', '.#...##..#.'])
     2
     '''
-    ncrosses = 0
-    p = 3
-    for row in rows[1:]:
-        if periodic(row)(p) == '#':
-            ncrosses += 1
-        p += 3
-
-    return ncrosses
+    return sum(1 for i, row in enumerate(rows[1:], start=1)
+               if periodic(row)(i * 3) == '#')
 
 
-def dec3_part2():
-    '''
-    Examples
-    '''
-    pass
+def dec3_part2(rows, rds):
+    def fun(rows, r, d):
+        return sum(1 for i, row in enumerate(rows[d::d], start=1)
+                   if periodic(row)(i * r) == '#')
+
+    ncrosses = [fun(rows, r, d) for r, d in rds]
+    prod = reduce(lambda a, b: a * b, ncrosses, 1)
+
+    return prod, ncrosses
 
 
 if __name__ == '__main__':
@@ -50,11 +43,18 @@ if __name__ == '__main__':
     sample_rows = [r for x in samplemap.split('\n') if (r := x.strip())]
 
     r_sample_part1 = dec3(sample_rows)
-    print(f'{r_sample_part1=}')
+    print(f'part1: {r_sample_part1=}')
 
     with open('./dec3-input.txt') as f:
         rows = [row.strip() for row in f]
 
-    r_part1  = dec3(rows)
+    r_part1 = dec3(rows)
 
-    print(f'{r_part1=}')
+    print(f'part1: {r_part1=}')
+
+    rds = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    prod_sample, r_sample_part2 = dec3_part2(sample_rows, rds)
+    print(f'part2: {prod_sample=}, {r_sample_part2=}')
+
+    prod, r_part2 = dec3_part2(rows, rds)
+    print(f'part2: {prod=}')
