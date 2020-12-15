dc504d4eab6ce3b1e92c04ab61b6460a50fcae5f
Mikkel Strange <mikkel.strange@gmail.com>
2020-12-15 20:35:22 +0100

    dec 7 part1

--- dec7/dec7.py
+++ dec7/dec7.py
@@ -1,52 +1,80 @@
-
-bagtypes = [
-    'light red',
-    'dark orange',
-    'bright white',
-    'muted yellow',
-    'shiny gold',
-    'dark olive',
-    'vibrant plum',
-    'faded blue',
-    'dotted black']
+from typing import Dict, List, Set, Tuple, Union
 
 
-def parse_r(r):
+# def parse_r(r: str) -> Union[None, Tuple[int, str]]:
+#     '''
+#     >>> parse_r('1 bright white bag')
+#     (1, 'bright white')
+#     >>> parse_r('2 muted yellow bags')
+#     (2, 'muted yellow')
+#     >>> parse_r('no other bags.') is None
+#     True
+#     '''
+#     xs = r.strip()
+#     if not xs[0].isnumeric():
+#         return None
+
+#     number, color1, color2, *_ = xs.split()
+#     count = int(number)
+#     bagtype = color1 + ' ' + color2
+#     return (count, bagtype)
+
+
+def parse_innerbag(r: str) -> Union[None, str]:
     '''
-    >>> parse_r('1 bright white bag')
-    (1, 'bright white')
-    >>> parse_r('2 muted yellow bags')
-    (2, 'muted yellow')
-    >>> parse_r('no other bags.') is None
+    >>> parse_innerbag('1 bright white bag')
+    'bright white'
+    >>> parse_innerbag('2 muted yellow bags')
+    'muted yellow'
+    >>> parse_innerbag('no other bags.') is None
     True
     '''
     xs = r.strip()
-    if not xs[0].isnumeric():
-        return
+    if xs == 'no other bags.':
+        return None
 
-    number, adjective, color, *_ = xs.split()
-    count = int(number)
-    bagtype = adjective + ' ' + color
-    return (count, bagtype)
+    _, color1, color2, *_ = xs.split()
+    bagtype = color1 + ' ' + color2
+    return bagtype
 
 
-def split_start_rest(rule):
+def into_outer_innerbags(rule: str) -> Tuple[str, List[str]]:
     '''
-    >>> rule = 'light red bags contain 1 bright white bag, 2 muted yellow bags.'
-    >>> split_start_rest(rule)
+    >>> r = 'light red bags contain 1 bright white bag, 2 muted yellow bags.'
+    >>> into_outer_innerbags(r)
     ('light red', ['1 bright white bag', '2 muted yellow bags.'])
-    >>> rule = 'faded blue bags contain no other bags.'
-    >>> split_start_rest(rule)
+    >>> r = 'faded blue bags contain no other bags.'
+    >>> into_outer_innerbags(r)
     ('faded blue', ['no other bags.'])
     '''
-    start, rest = rule.split('contain')
-    start_bag = start.replace('bags', '').strip()
-    rest_split = [x.strip() for x in rest.split(',')]
-    return start_bag, rest_split
+    outer, rest = rule.split('contain')
+    outerbag = outer.replace('bags', '').strip()
+    innerbags = [x.strip() for x in rest.split(',')]
+    return outerbag, innerbags
 
 
-def solve_sample():
-    pass
+def rules2map(rules: List[str]) -> Dict[str, List[str]]:
+    bags = {}
+    for rule in rules:
+        outerbag, innerbags = into_outer_innerbags(rule)
+        bags[outerbag] = [parsed for innerbag in innerbags
+                          if (parsed := parse_innerbag(innerbag))]
+    return bags
+
+
+def bag_in_outerbag(bagmap: Dict[str, List[str]], outerbag, bag) -> bool:
+    innerbags = bagmap[outerbag]
+    return (bag in innerbags or
+            any(bag_in_outerbag(bagmap, b, bag) for b in innerbags))
+
+
+def outerbagscontaining(bag, rules) -> Set[str]:
+    bagmap = rules2map(rules)
+    bags = set()
+    for outerbag in bagmap.keys():
+        if bag_in_outerbag(bagmap, outerbag, bag):
+            bags.add(outerbag)
+    return bags
 
 
 def readsample():
@@ -54,9 +82,18 @@ def readsample():
         return [line.strip() for line in f]
 
 
+def readpuzzle():
+    with open('./puzzle-input.txt') as f:
+        return [line.strip() for line in f]
+
+
 if __name__ == '__main__':
     lines = readsample()
-    line = lines[0]
+    outerbags = outerbagscontaining('shiny gold', lines)
+    nouterbags = len(outerbags)
+    print(f'{nouterbags=}')
 
-    for line in lines:
-        print(split_start_rest(line))
+    lines2 = readpuzzle()
+    outerbags2 = outerbagscontaining('shiny gold', lines2)
+    nouterbags2 = len(outerbags2)
+    print(f'{nouterbags2=}')
