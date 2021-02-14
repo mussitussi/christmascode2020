d2fd4bac20f584b1cf85181b0ee1a70aea934bd5
Mikkel Strange <mikkel.strange@gmail.com>
2021-02-14 10:39:20 +0100

    added fast O(N) version for 2 item sum rule probelm

--- dec1/dec1.py
+++ dec1/dec1.py
@@ -1,3 +1,6 @@
+from typing import Dict, Iterable
+
+
 def dec1_2(xs):
     '''
     The product of two expenses that sums to 2020
@@ -10,6 +13,36 @@ def dec1_2(xs):
         for x2 in xs[i + 1:]:
             if x1 + x2 == 2020:
                 return x1 * x2
+
+
+def dec1_2_order_n(xs: Iterable[int], t: int):
+    '''
+    O(N) version instead of naive O(N2), using a dictionary
+    which has O(1) lookup time to resure checks
+    "rewrite" sum rule:
+    xi + xj = t, i /= j <=>
+    xi = t - xj, i /=j
+    and store t - xj as we go along for fast lookups
+    Example:
+    xs = [5, 1, 10, 9]
+    t = 10
+    steps:
+    prev = {}
+    x0 = 5
+    prev.get(x0) -> None -> prev = {t - x0: x0} = {5: 5}
+    x1 = 1
+    prev.get(x1) -> None -> prev = {9: 1, 5: 5}
+    x2 = 10
+    prev.get(x2) -> None -> prev = {0: 10, 9: 1, 5: 5}
+    x3 = 9
+    prev.get(x3) -> 1 -> return 9 * 1
+    '''
+    lookup: Dict[int, int] = {}
+    for xi in xs:
+        xj = lookup.get(xi)
+        if xj is not None:
+            return xi * xj
+        lookup[t - xi] = xi
 
 
 def dec1_3(xs):
@@ -37,10 +70,13 @@ if __name__ == '__main__':
         ys = [int(line.strip()) for line in f]
 
     r2 = dec1_2(ys)
-    print('answer (2 expenses)', r2)
+    print('answer (O(N^2))(2 expenses)', r2)
+
+    r2_on = dec1_2_order_n(ys, 2020)
+    print('answer (O(N))(2 expenses)', r2_on)
 
     r3_sample = dec1_3(xs)
-    print('sample answer (2 expenses):', r3_sample)
+    print('sample answer (3 expenses):', r3_sample)
 
     r3 = dec1_3(ys)
     print('answer (3 expenses)', r3)
